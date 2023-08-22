cc07c9904bb1f26d317b666caed3fa3cf611505f
nonsodaniel <nonsodaniel07@gmail.com>
2023-08-22 16:08:43 +0100

    Remove unused configs

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -9,7 +9,6 @@ const MusicPlayer = () => {
   const player = useMusicPlayer();
   const { song } = useFetchBySongId(player.activeId!);
   const songUrl = useLoadSongUrl(song!); // song! is used because 'song' can never be null or undefined This is known as the "non-null assertion operator."
-  console.log({ song, songUrl, player });
   if (!song || !songUrl || !player.activeId) return null;
   console.log({ songUrl, song });
 
