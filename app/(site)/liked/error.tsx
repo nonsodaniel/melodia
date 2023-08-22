c00683d4534c33c3b471ace32ac0d6c75637904e
nonsodaniel <nonsodaniel07@gmail.com>
2023-08-22 20:40:06 +0100

    Added loading && error state

---
+++ app/(site)/liked/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Box from "@/components/Box";
+import React from "react";
+
+const Error = () => {
+  return (
+    <Box className="flex items-center justify-center h-full">
+      <div className="text-neutral-400">An error occured!</div>
+    </Box>
+  );
+};
+
+export default Error;
