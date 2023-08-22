2f89b17a1604dc46ae57c39f211070798a0b3867
nonsodaniel <nonsodaniel07@gmail.com>
2023-08-22 16:37:07 +0100

    Songs can now  be played from search results

--- components/SearchContent.tsx
+++ components/SearchContent.tsx
@@ -3,11 +3,13 @@
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface ISearchContentProps {
   songs: Song[];
 }
 const SearchContent = ({ songs }: ISearchContentProps) => {
+  const onPlay = useOnPlay(songs);
   if (!songs.length) {
     return (
       <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
@@ -20,7 +22,11 @@ const SearchContent = ({ songs }: ISearchContentProps) => {
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem key={song.id} song={song} />
+            <MediaItem
+              key={song.id}
+              song={song}
+              onClick={(id: string) => onPlay(id)}
+            />
           </div>
           <LikeButton songId={song.id} />
         </div>
